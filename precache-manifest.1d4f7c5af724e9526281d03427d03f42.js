00f0eedaec5660f2d3260de67b2cf470a782168c
Meghana Madala <1313meghana@gmail.com>
2020-08-14 10:01:33 +0530

    Updates

---
+++ precache-manifest.1d4f7c5af724e9526281d03427d03f42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4899ca6fa5f4ecc571a",
+    "url": "/calculatorReact/static/css/main.7909c433.chunk.css"
+  },
+  {
+    "revision": "e4899ca6fa5f4ecc571a",
+    "url": "/calculatorReact/static/js/main.e4899ca6.chunk.js"
+  },
+  {
+    "revision": "8256f6d2eb5559535093",
+    "url": "/calculatorReact/static/js/1.8256f6d2.chunk.js"
+  },
+  {
+    "revision": "34eab25833089d7c6891",
+    "url": "/calculatorReact/static/js/runtime~main.34eab258.js"
+  },
+  {
+    "revision": "dde6918da6942004f4d43e57ca08e145",
+    "url": "/calculatorReact/index.html"
+  }
+];
